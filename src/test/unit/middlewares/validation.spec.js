df89ac96f2e1e453b17b079eef04e17e9568850e
kostya <paholokkostantin@gmail.com>
2026-08-31 14:09:53 +0300

    add validation middleware unit tests

--- src/test/unit/middlewares/validation.spec.js
+++ src/test/unit/middlewares/validation.spec.js
@@ -15,4 +15,67 @@ describe('Validation middleware', () => {
 
     expect(middlewareFunc).toThrow(err)
   })
+
+  it('Should throw an error when required field is missing', () => {
+    const mockRequest = {
+      body: {
+        lastName: 'Test',
+        email: 'Test@example.com',
+        password: 'Test123!',
+        role: 'student'
+      }
+    }
+
+    const middlewareFunc = () => middlewareToTest(mockRequest, mockResponse, mockNextFunc)
+
+    expect(middlewareFunc).toThrow()
+  })
+
+  it('Should throw an error when email format is invalid', () => {
+    const mockRequest = {
+      body: {
+        firstName: 'Test',
+        lastName: 'Test',
+        email: 'invalid-email',
+        password: 'Test123!',
+        role: 'student'
+      }
+    }
+
+    const middlewareFunc = () => middlewareToTest(mockRequest, mockResponse, mockNextFunc)
+
+    expect(middlewareFunc).toThrow()
+  })
+
+  it('Should throw an error when role is invalid', () => {
+    const mockRequest = {
+      body: {
+        firstName: 'Test',
+        lastName: 'Test',
+        email: 'test@example.com',
+        password: 'Test123!',
+        role: 'invalid-role'
+      }
+    }
+
+    const middlewareFunc = () => middlewareToTest(mockRequest, mockResponse, mockNextFunc)
+
+    expect(middlewareFunc).toThrow()
+  })
+
+  it('Should call next when body is valid', () => {
+    const mockRequest = {
+      body: {
+        firstName: 'Test',
+        lastName: 'test',
+        email: 'test@example.com',
+        password: 'Test123!',
+        role: 'student'
+      }
+    }
+
+    middlewareToTest(mockRequest, mockResponse, mockNextFunc)
+
+    expect(mockNextFunc).toHaveBeenCalled()
+  })
 })
